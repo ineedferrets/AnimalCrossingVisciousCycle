26447ceeb17fb3823f28ec4c32af74a84b92994d
Joseph Hesketh <joe.m.hesketh@gmail.com>
2018-05-21 13:06:21 +0100

    Simplified Signal Sending
    Sending signals to Unity has been simplified and pretty much works.

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -1,34 +1,38 @@
-#include <SoftwareSerial.h>
-#include "SerialCommand.h"
-SerialCommand sCmd;
+//#define NATIVE_USB
+
+//#include "SoftwareSerial.h"
+//#include "SerialCommand.h"
+//SerialCommand sCmd;
 
 float leftWheelAngVel;
 float rightWheelAngVel;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  while(!Serial);
 
-  sCmd.addCommand("PING", pingHandler);
-  sCmd.addCommand("REQUEST", requestHandler);
+  Serial.begin(1200);
+
+  Serial.write("Begun running");
+  
+  //SerialUSB.begin(1);
+
+  //sCmd.addCommand("PING", pingHandler);
+  //sCmd.addCommand("REQUEST", requestHandler);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(Serial.available() > 0)
-    sCmd.readSerial();
+  Serial.write("Running...\n");
+  //SerialUSB.println("I'm pringint a lint");
 }
 
 void pingHandler()
 {
-  Serial.println("PONG");
+  //Serial.println("PONG");
 }
 
 void requestHandler()
 {
-	String outputString = String(String(leftWheelAngVel) + " " + String(rightWheelAngVel));
+	//String outputString = String(String(leftWheelAngVel) + " " + String(rightWheelAngVel));
   
-	Serial.println(outputString);
+	//Serial.println(outputString);
 }
 
