6cb92d8985e3b212a11c8eec35837adca6bbf1c7
Joseph Hesketh <joe.m.hesketh@gmail.com>
2018-05-23 16:14:54 +0100

    Deleted Game Objects

---
+++ Assets/SelfDestruct.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour {
+
+    [Range(1f,20f)]
+    public float deathTime = 4f;
+
+    private float timer = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer > deathTime) {
+            Destroy(gameObject);
+        }
+	}
+}
