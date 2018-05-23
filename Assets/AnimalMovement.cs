7920ae4af18f41fe7989e03f0710a875de8e3127
Joseph Hesketh <joe.m.hesketh@gmail.com>
2018-05-23 15:35:24 +0100

    Animal Crossing
    New and Improved

---
+++ Assets/AnimalMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalMovement : MonoBehaviour {
+
+    [Range(0f, 20f)]
+    public float maxAngularVelocity = 5f;
+
+    [Range(0f, 10f)]
+    public float minAngularVelocity = 3f;
+
+    [Range(0f,20f)]
+    public float maxLinearVelocity = 8f;
+
+    [Range(0f, 10f)]
+    public float minLinearVelocity = 6f;
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Rigidbody>().velocity = transform.forward * Random.RandomRange(minLinearVelocity, maxLinearVelocity);
+        this.GetComponent<Rigidbody>().angularVelocity = new Vector3(Random.RandomRange(-1.0f, 1.0f), 
+            Random.RandomRange(-1.0f, 1.0f), 
+            Random.RandomRange(-1.0f, 1.0f)) * Random.RandomRange(minAngularVelocity, maxAngularVelocity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/SpawnObject.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour {
+
+    public GameObject[] prefabAnimals;
+
+    [Range(0.1f, 20)]
+    public float maxTimeToSpawn = 6f;
+
+    [Range(0.1f, 10f)]
+    public float minTimeToSpawn = 2f;
+
+    private float timeToSpawn;
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+        ResetTimer();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        if (timer > timeToSpawn) {
+            GameObject newObj = Instantiate(prefabAnimals[Random.RandomRange(0, prefabAnimals.Length)], transform.position, Quaternion.identity, transform);
+            newObj.transform.Rotate(transform.rotation.eulerAngles);
+            ResetTimer();
+        }
+	}
+
+    private void ResetTimer() {
+        timer = 0f;
+        timeToSpawn = Random.RandomRange(minTimeToSpawn, maxTimeToSpawn);
+    }
+}
